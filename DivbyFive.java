b6556859f70c4cc686d6a9897a0d822bcce536ae
jd6132 <jd6132@srmist.edu.in>
2026-03-03 20:47:03 +0530

    DivbyFive

---
+++ DivbyFive.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DivbyFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        boolean result = (number % 5 == 0);
+
+        System.out.println("Is the number " + number + " divisible by 5? " + result);
+
+        sc.close();
+    }
+}
